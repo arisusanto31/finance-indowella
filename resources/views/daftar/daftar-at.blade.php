1149368588cedc38ffa26bd02e7d2962b94d6b98
ari susanto <email_anda@example.com>
2025-03-27 13:45:57 +0700

    update table

--- resources/views/daftar/daftar-at.blade.php
+++ resources/views/daftar/daftar-at.blade.php
@@ -1,87 +1,112 @@
 <x-app-layout>
   <style>
-    th, td {
+    table.table {
+      border-collapse: separate;
+      border-spacing: 0;
+      width: max-content;
+      min-width: 1500px;
+      table-layout: fixed;
+      background-color: white;
+    }
+
+    table th, table td {
+      border: 1px solid #dee2e6;
+      box-sizing: border-box;
+      padding: 0.5rem;
       white-space: nowrap;
+      vertical-align: middle;
+      text-align: center;
       background-color: white !important;
+      color: #000;
+      overflow: hidden;
+      text-overflow: ellipsis;
     }
 
-   
-    .sticky-col-1 {
-      position: sticky;
-      left: 0;
-      width: 50px;
-      min-width: 50px;
-      max-width: 50px;
-      z-index: 5;
-      text-align: center;
+    thead.table-light th.bg-primary {
+      background-color: #007bff !important;
+      color: white;
     }
 
-    .sticky-col-2 {
+    /* Sticky columns */
+    .sticky-col-1, .sticky-col-2, .sticky-col-3, .sticky-col-4, .sticky-col-5 {
       position: sticky;
-      left: 50px;
-      width: 180px;
-      min-width: 180px;
-      max-width: 180px;
-      z-index: 6;
+      background-color: white !important;
+      z-index: 30;
+      border-right: 1px solid #dee2e6;
     }
 
-    .sticky-col-3 {
-      position: sticky;
-      left: 230px; 
-      width: 80px;
-      min-width: 80px;
-      max-width: 80px;
-      z-index: 7;
-      text-align: center;
-    }
-
-    .sticky-col-4 {
-      position: sticky;
-      left: 310px; 
-      width: 100px;
-      min-width: 100px;
-      max-width: 100px;
-      z-index: 8;
-      text-align: center;
-    }
+    .sticky-col-1 { left: 0px;   width: 60px;  min-width: 60px;  max-width: 60px;  }
+    .sticky-col-2 { left: 60px;  width: 180px; min-width: 180px; max-width: 180px; }
+    .sticky-col-3 { left: 240px; width: 80px;  min-width: 80px;  max-width: 80px;  }
+    .sticky-col-4 { left: 320px; width: 100px; min-width: 100px; max-width: 100px; }
+    .sticky-col-5 { left: 420px; width: 100px; min-width: 100px; max-width: 100px; }
   </style>
 
-  <div class="p-4">
-    <div class="table-responsive">
-      <table class="table table-bordered">
-        <thead class="table-light">
-          <tr>
-            <th class="sticky-col-1">No</th>
-            <th class="sticky-col-2">Nama Barang</th>
-            <th class="sticky-col-3">Qty</th>
-            <th class="sticky-col-4">Tahun</th>
-            <th>Bulan</th>
-            <th>Nilai Perolehan</th>
-            <th>Akumulasi Penyusutan</th>
-            <th>Nilai Buku</th>
-            <th>Penyusutan Jan</th>
-            <th>Penyusutan Feb</th>
-            <th>Penyusutan Mar</th>
-            <th>Penyusutan Apr</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="sticky-col-1">1</td>
-            <td class="sticky-col-2">Printer Sharkpos</td>
-            <td class="sticky-col-3">2</td>
-            <td class="sticky-col-4">2025</td>
-            <td class="sticky-col-5">Feb</td>
-            <td>474.000</td>
-            <td>0</td>
-            <td>474.000</td>
-            <td>9.875</td>
-            <td>9.875</td>
-            <td>9.875</td>
-            <td>9.875</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+  @php
+    $table1 = [
+      'title' => 'Table 1',
+      'headers' => ['No', 'Tanah', 'Qty', 'Tahun', 'Bulan', 'Periode', 'Nilai Perolehan', 'Akum. Peny', 'Mutasi Pembelian', 'Nilai Perolehan',
+        'Penyusutan Januari', 'Penyusutan Februari', 'Penyusutan Maret', 'Penyusutan April', 'Penyusutan Mei', 'Penyusutan Juni', 'Penyusutan Juli',
+        'Penyusutan Agustus','Penyusutan September','Penyusutan Oktober','Penyusutan November','Penyusutan Desember',
+        'Total Penyusutan','Total Akumulasi Penyusutan','Nilai Buku','Keterangan'],
+      'data' => [
+        ['1', 'Printer Sharkpos', 2, 2025, 'Feb', '1 Tahun', '10.000.000', '1.000.000', '0', '10.000.000',
+         '100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000',
+         '1.200.000', '2.000.000', '8.000.000', 'Aktif']
+      ],
+      'subtotal' => array_pad(['-', '-', '-', '-', 'Subtotal', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 26, '-'),
+    ];
+
+    $table2 = [
+      'title' => 'Table 2',
+      'headers' => ['No', 'Truck', 'Qty', 'Tahun', 'Bulan', 'Periode', 'Nilai Perolehan', 'Akum. Peny', 'Mutasi Pembelian', 'Nilai Perolehan',
+        'Penyusutan Januari', 'Penyusutan Februari', 'Penyusutan Maret', 'Penyusutan April', 'Penyusutan Mei', 'Penyusutan Juni', 'Penyusutan Juli',
+        'Penyusutan Agustus','Penyusutan September','Penyusutan Oktober','Penyusutan November','Penyusutan Desember',
+        'Total Penyusutan','Total Akumulasi Penyusutan','Nilai Buku','Keterangan'],
+      'data' => [
+        ['1', 'Printer Sharkpos', 2, 2025, 'Feb', '1 Tahun', '10.000.000', '1.000.000', '0', '10.000.000',
+         '100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000',
+         '1.200.000', '2.000.000', '8.000.000', 'Aktif']
+      ],
+      'subtotal' => ['-', 'Subtotal', '-', '-', '-', '250.000.000', '12.500', '-'],
+    ];
+
+    $table3 = [
+      'title' => 'Table 3',
+      'headers' => ['No', 'Mobil/Motor', 'Qty', 'Tahun', 'Bulan', 'Nilai Beli', 'Penyusutan', 'Catatan Tambahan'],
+      'data' => [
+        ['1', 'Canter 125PS', 1, 2023, 'Jan', '250.000.000', '12.500', 'Ready'],
+      ],
+      'subtotal' => ['-', 'Subtotal', '-', '-', '-', '250.000.000', '12.500', '-'],
+    ];
+    $table4 = [
+      'title' => 'Table 4',
+      'headers' => ['No', 'P', 'Qty', 'Tahun', 'Bulan', 'Periode', 'Nilai Perolehan', 'Akum. Peny', 'Mutasi Pembelian', 'Nilai Perolehan',
+        'Penyusutan Januari', 'Penyusutan Februari', 'Penyusutan Maret', 'Penyusutan April', 'Penyusutan Mei', 'Penyusutan Juni', 'Penyusutan Juli',
+        'Penyusutan Agustus','Penyusutan September','Penyusutan Oktober','Penyusutan November','Penyusutan Desember',
+        'Total Penyusutan','Total Akumulasi Penyusutan','Nilai Buku','Keterangan'],
+      'data' => [
+        ['1', 'Printer Sharkpos', 2, 2025, 'Feb', '1 Tahun', '10.000.000', '1.000.000', '0', '10.000.000',
+         '100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000','100.000',
+         '1.200.000', '2.000.000', '8.000.000', 'Aktif']
+      ],
+      'subtotal' => ['-', 'Subtotal', '-', '-', '-', '250.000.000', '12.500', '-'],
+    ];
+    function normalizeTable(&$table) {
+      $count = count($table['headers']);
+      foreach ($table['data'] as &$row) {
+        $row = array_pad($row, $count, '-');
+      }
+      $table['subtotal'] = array_pad($table['subtotal'], $count, '-');
+    }
+
+    normalizeTable($table1);
+    normalizeTable($table2);
+    normalizeTable($table3);
+  @endphp
+
+  <x-dynamic-table :title="$table1['title']" :headers="$table1['headers']" :data="$table1['data']" :subtotal="$table1['subtotal']" />
+  <x-dynamic-table :title="$table2['title']" :headers="$table2['headers']" :data="$table2['data']" :subtotal="$table2['subtotal']" />
+  <x-dynamic-table :title="$table3['title']" :headers="$table3['headers']" :data="$table3['data']" :subtotal="$table3['subtotal']" />
+  <x-dynamic-table :title="$table4['title']" :headers="$table4['headers']" :data="$table4['data']" :subtotal="$table4['subtotal']" />
 </x-app-layout>
