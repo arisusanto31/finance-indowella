7c957e35cd2c0189feedc060d12e517380c7002f
iqomchanif <chanif.qomaruddin@gmail.com>
2025-08-11 11:31:25 +0700

    bismillah

--- resources/views/invoice/modal/_invoice-detail.blade.php
+++ resources/views/invoice/modal/_invoice-detail.blade.php
@@ -68,7 +68,7 @@
                                                 <p>{{ $item->date }} -
                                                     <strong>{{ $item->code_group_name }}</strong> :
                                                     {{ format_price($item->amount_journal) }} <span
-                                                        class="fs-8">[journal_id : {{ $item->journal_id }}, journal_number= {{$item['journal_number']}}
+                                                        class="fs-8">[journal_id : {{ $item->journal_id }}, journal_number= {{$item['journal_number']}},
                                                         kartu_id= {{ $item->kartu_id }}]
                                                     
                                                     </span>
