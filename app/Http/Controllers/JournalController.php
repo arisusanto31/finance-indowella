0407637a0047671c0b8c5ac44691e59cd4cfae3f
iqomchanif <chanif.qomaruddin@gmail.com>
2026-04-09 14:24:13 +0700

    ok

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -347,7 +347,7 @@ class JournalController extends Controller
         }
         return [
             'status' => 1,
-            'msg' => $journal
+            'journal' => $journal
         
         ];
     }
