0a2a4ea40b0db10ca74d5afc4d63fe27ca193603
iqomchanif <chanif.qomaruddin@gmail.com>
2026-04-24 13:29:50 +0700

    ok

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -54,7 +54,7 @@ class JournalController extends Controller
             //breati ga ada permintaan date, kita cari di month dan year ya
             $month = getInput('month') ? toDigit(getInput('month'), 2) : Date('m');
             $year = getInput('year') ? getInput('year') : Date('Y');
-            $date = createCarbon($year . '-' . $month . '-01')->format('Y-m-t 23:59:54');
+            $date = createCarbon($year . '-' . $month . '-01')->format('Y-m-t 23:59:59');
         }
         $query = ChartAccount::getRincianNeracaAt($date);
         if ($query['status'] == 0)
