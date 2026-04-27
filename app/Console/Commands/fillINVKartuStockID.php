746fdf4d2bdf9de9dc5eb5aa4fb1eff0c65befa2
iqomchanif <chanif.qomaruddin@gmail.com>
2026-04-27 10:02:03 +0700

    ok

---
+++ app/Console/Commands/fillINVKartuStockID.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\InvoicePurchaseDetail;
+use App\Models\KartuStock;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class fillINVKartuStockID extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fill:inv-kartu-stock-id {invid}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $invid = $this->argument('invid');
+        $inv = InvoicePurchaseDetail::find($invid);
+        $ks = KartuStock::where('purchase_order_id', $inv->id)->first();
+        if ($ks) {
+            $ks->kartu_stock_id = $ks->id;
+        }
+        if (!$ks) {
+            $ks = KartuStock::leftJoin('journals', 'journals.id', 'kartu_stocks.journal_id')
+                ->where('journals.book_journal_id', bookID())
+                ->where('kartu_stocks.stock_id', $inv->stock_id)
+                ->where('journals.description', 'like', '%' . $inv->invoice_pack_number . '%')
+                ->select('kartu_stocks.id as kartu_stock_id', 'kartu_stocks.mutasi_rupiah_total', 'journals.id as journal_id', 'journals.journal_number', 'journals.index_date_group')
+                ->get();
+            tampilkanTableTerminal($ks,[
+                'kartu_stock_id' => 'center',
+                'mutasi_rupiah_total' => 'right',
+                'journal_id' => 'center',
+                'journal_number' => 'center',
+                'index_date_group' => 'center'
+            ],$this);
+            if (count($ks) == 1) {
+                $ks = $ks->first();
+            } else {
+                $ks = collect($ks)->where('mutasi_rupiah_total', $inv->total_price)->first();
+            }
+        }
+        if ($ks) {
+            $inv->kartu_stock_id = $ks->kartu_stock_id;
+            $inv->index_date = self::getNextIndexDate(Carbon::createFromFormat('ymdHis', $ks->index_date_group));
+            $inv->index_date_group = $ks->index_date_group;
+            $inv->journal_id = $ks->journal_id;
+            $inv->journal_number = $ks->journal_number;
+            $inv->save();
+        }
+    }
+}
