bd69f2991d3cd50c2262f67e97b6730cd3897406
iqomchanif <chanif.qomaruddin@gmail.com>
2026-04-28 06:35:40 +0700

    ok

--- app/Console/Commands/CekBahanAvailableDibebankan.php
+++ app/Console/Commands/CekBahanAvailableDibebankan.php
@@ -34,54 +34,25 @@ class CekBahanAvailableDibebankan extends Command
         $catid = $this->argument('catid');
         $year = $this->argument('year', 2025);
         $catid = explode(',', $catid);
+        $stocknames = Stock::whereIn('category_id', $catid)->pluck('name', 'id')->all();
+        $start = createCarbon($year . '-01-01 00:00:00')->format('ymdHis000');
         $allstockid = Stock::whereIn('category_id', $catid)->pluck('id')->toArray();
-        $lasthpp = KartuStock::whereIn('index_date', function ($q) use ($allstockid) {
-            $q->selectRaw('max(index_date)')->from('kartu_stocks')
-                ->whereIn('stock_id', $allstockid)->groupBy('stock_id');
-        })->where('saldo_qty_backend', '>', 0)->select('stock_id', 'saldo_qty_backend', DB::raw('saldo_rupiah_total/saldo_qty_backend as hpp'))->get()->keyBy('stock_id');
-        $stockname = Stock::whereIn('id', $allstockid)->pluck('name', 'id')->all();
-        for ($month = 12; $month > 0; $month--) {
-            $indexDateAwal = createCarbon($year . '-' . toDigit($month, 2) . '-01')->startOfMonth()->format('ymdHis000');
-            $indexDateAkhir = createCarbon($year . '-' . toDigit($month, 2) . '-01')->endOfMonth()->format('ymdHis999');
-            // $this->info('cari mutasi dari '.$indexDateAwal.' sampai '.$indexDateAkhir);
-            $mutasi = KartuStock::where('index_date', '>', $indexDateAwal)->where('index_date', '<', $indexDateAkhir)
+        $allMutasi = KartuStock::where('index_date', '>', $start)
+            ->whereIn('stock_id', $allstockid)->selec('saldo_qty_backend', 'saldo_rupiah_total', 'stock_id')->get()->groupBy('stock_id')
+            ->map(function ($item) use ($stocknames) {
+                $min = collect($item)->min('saldo_qty_backend');
+                return [
+                    'stock_id' => $item[0]->stock_id,
+                    'stock_name' => $stocknames[$item[0]->stock_id] ?? '',
+                    'available' => $min,
 
-                ->whereIn('stock_id', $allstockid)
-                ->where('mutasi_qty_backend', '>', 0)->select(DB::raw('sum(mutasi_qty_backend) as total_pembelian'), 'stock_id')->groupBy('stock_id')
-                ->get();
-            // tampilkanTableTerminal($mutasi,[
-            //     'stock_id' => 'center',
-            //     'total_pembelian' => 'right'
-            // ], $this);
-            $mutasi = $mutasi->pluck('total_pembelian', 'stock_id')->all();
-            $datas = [];
-            foreach ($lasthpp as $stockid => $data) {
-
-                $datas[] = [
-                    'month' => $year . '-' . $month,
-                    'stock_id' => $stockid,
-                    'stock_name' => $stockname[$stockid] ?? $stockid,
-                    'available' => $lasthpp[$stockid]->saldo_qty_backend,
-                    'hpp' => $data->hpp,
-                    'total_nilai' => $lasthpp[$stockid]->saldo_qty_backend * $data->hpp,
-                    'total_pembelian' => isset($mutasi[$stockid]) ? $mutasi[$stockid] : 0
                 ];
-                $lasthpp[$stockid]->saldo_qty_backend -= isset($mutasi[$stockid]) ? $mutasi[$stockid] : 0;
-                if ($lasthpp[$stockid]->saldo_qty_backend <= 0) {
-                    // $lasthpp[$stockid]->saldo_qty_backend = 0;
-                    //buang array stock id dalam lasthpp
-                    unset($lasthpp[$stockid]);
-                }
-            }
-            tampilkanTableTerminal($datas, [
-                'month' => 'center',
-                'stock_id' => 'center',
-                'stock_name' => 'center',
-                'available' => 'right',
-                'hpp' => 'right',
-                'total_nilai' => 'right',
-                'total_pembelian' => 'right'
-            ], $this);
-        }
+            })->values();
+
+        tampilkanTableTerminal($allMutasi, [
+            'stock_id' => 'center',
+            'stock_name' => 'center',
+            'available' => 'right',
+        ], $this);
     }
 }
