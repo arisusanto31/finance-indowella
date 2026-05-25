b5c694d65605a81b8434300321deb48e6a645ab5
iqomchanif <chanif.qomaruddin@gmail.com>
2026-05-26 05:44:03 +0700

    ok

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -602,13 +602,18 @@ class JournalController extends Controller
             CustomLogger::log('invoicing','info','jurnal- selesai buat jurnal. time: '.(microtime(true) - $time).' detik');
             foreach ($allJournals as $journal) {
                 $journal->updateLawanCode();
+                CustomLogger::log('invoicing','info','jurnal- update lawan code '.$journal->code_group.'. time: '.(microtime(true) - $time).' detik');
+                
                 $st = $journal->createDetailKartuInvoice();
                 if ($st['status'] == 0) {
                     throw new \Exception('gagal membuat detail kartu invoice: ' . $st['msg']);
                 }
+                CustomLogger::log('invoicing','info','jurnal- selesai membuat detail kartu invoice '.$journal->code_group.'. time: '.(microtime(true) - $time).' detik');
+                
                 if ($isBackDate == 1 && !$noRecalculate) {
                     $journal->calculateJournalNext(false);
                 }
+                CustomLogger::log('invoicing','info','jurnal- selesai recalculate journal '.$journal->code_group.'. time: '.(microtime(true) - $time).' detik');
             }
             if ($isLockIntern == 1) {
                 $lockManager->releaseAll();
