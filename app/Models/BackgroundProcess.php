7d2514369915ff98a944003a810d1da08957f8e3
iqomchanif <chanif.qomaruddin@gmail.com>
2026-05-25 13:11:57 +0700

    bismillah

--- app/Models/BackgroundProcess.php
+++ app/Models/BackgroundProcess.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class BackgroundProcess extends Model
 {
     //
-    protected $table= 'background_process';
+    protected $table = 'background_process';
     protected $fillable = [
         'monitoring_url',
         'progress',
@@ -18,4 +19,21 @@ class BackgroundProcess extends Model
         'status',
     ];
 
+    protected static function booted()
+    {
+        static::addGlobalScope('journal', function ($query) {
+            $from = $query->getQuery()->from ?? 'background_process'; // untuk dukung alias `j` kalau pakai from('journals as j')
+            if (Str::contains($from, ' as ')) {
+                [$table, $alias] = explode(' as ', $from);
+                $alias = trim($alias);
+            } else {
+                $alias = $from;
+            }
+
+            $query->where(function ($q) use ($alias) {
+                $q->whereNull("{$alias}.book_journal_id")
+                    ->orWhere("{$alias}.book_journal_id", bookID());
+            });
+        });
+    }
 }
