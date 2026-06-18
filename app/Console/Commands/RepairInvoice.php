2d1f0d43b0aa508dd8b12aaa5f071255921a3d2a
iqomchanif <chanif.qomaruddin@gmail.com>
2026-06-18 07:49:21 +0700

    bismillah

--- app/Console/Commands/RepairInvoice.php
+++ app/Console/Commands/RepairInvoice.php
@@ -31,22 +31,35 @@ class RepairInvoice extends Command
         $month = $this->argument('month');
         $bookid = $this->argument('bookid');
         Session::put('book_journal_id', $bookid);
-        $month.= '-01';
+        $month .= '-01';
         $dateAwal = createCarbon($month)->startOfMonth();
         $dateAkhir = createCarbon($month)->endOfMonth();
-        $sales = SalesOrder::whereBetween('created_at', [$dateAwal, $dateAkhir])->where('status_delivery','<>','TERKIRIM 100%')->get();
+        $sales = SalesOrder::whereBetween('created_at', [$dateAwal, $dateAkhir])->where('status_delivery', '<>', 'TERKIRIM 100%')->get();
         $this->info('Found ' . $sales->count() . ' sales orders for month: ' . $month);
+        $totalcount = $sales->count();
+        $i = 0;
+        $countsuccess = 0;
+        $countfailed = 0;
         foreach ($sales as $salesOrder) {
+            $i++;
             if (!$salesOrder) {
                 $this->error('Sales order not found');
-                return;
+                $countfailed++;
+
+                // return;
+            } else {
+                $st = $salesOrder->repairInvoice();
+                if (!$st) {
+                    $this->error('Failed to repair invoice for sales order ' . $salesOrder->sales_order_number);
+                    $countfailed++;
+                    // return;
+                } else {
+                    $countsuccess++;
+                }
+                $this->info('Invoice repaired successfully for sales order ' . $salesOrder->sales_order_number);
             }
-            $st = $salesOrder->repairInvoice();
-            if (!$st) {
-                $this->error('Failed to repair invoice for sales order ' . $salesOrder->sales_order_number);
-                return;
-            }
-            $this->info('Invoice repaired successfully for sales order ' . $salesOrder->sales_order_number);
+            $progress = round(($i / $totalcount) * 100, 2);
+            $this->info('Progress: ' . $progress . '%' . ' success : ' . $countsuccess . ' failed : ' . $countfailed);
         }
     }
 }
