990a34af992cf7a91600cd2c99de30c988d2eca3
iqomchanif <chanif.qomaruddin@gmail.com>
2026-07-06 18:31:19 +0700

    ok

---
+++ database/migrations/2026_07_06_174025_insert_on_sales_order_details_sales_id.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('sales_order_details',function($table){
+           $table->index('sales_order_id');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sales_order_details',function($table){
+            $table->dropIndex(['sales_order_id']);
+        });
+    }
+};
