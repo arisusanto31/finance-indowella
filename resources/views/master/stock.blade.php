3de1ffcefe807c7ddae6f0339699a4808818a896
iqomchanif <chanif.qomaruddin@gmail.com>
2026-07-09 13:07:47 +0700

    bismillah

--- resources/views/master/stock.blade.php
+++ resources/views/master/stock.blade.php
@@ -339,7 +339,7 @@
                                                                         <div class="row">
                                                                             <div class="col-xs-12" style="position:relative; width:100%">
                                                                                 <span class="unit-form${item.id}" style="position:absolute; right:20px; top:7px; color:#bbb"> ${item.unit_backend}</span>
-                                                                                <input class="form-control" placeholder="konversi" value="${unit.konversi}" onchange="updateKonversi(${unit.id}, this.value)" />
+                                                                                <input class="" placeholder="konversi" value="${unit.konversi}" onchange="updateKonversi(${unit.id}, this.value)" />
                                                                                 <button onclick="deleteKonversi(${unit.id})"> <i class="fas fa-close"></i></button>
                                                                             </div>
                                                                         </div>
