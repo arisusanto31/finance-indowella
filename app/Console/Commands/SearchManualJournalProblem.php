126217f1e327eb611a150210b8adee5755db12b9
iqomchanif <chanif.qomaruddin@gmail.com>
2026-07-15 07:36:04 +0700

    bismillah

--- app/Console/Commands/SearchManualJournalProblem.php
+++ app/Console/Commands/SearchManualJournalProblem.php
@@ -33,15 +33,19 @@ class SearchManualJournalProblem extends Command
         $lastSaldo = [];
         $problemsLag = [];
         $problemsManual = [];
-        $journals = Journal::select(
+        $fixJournals = Journal::where('book_journal_id', $this->argument('bookid'))->select(
             'code_group',
+            'book_journal_id',
             'index_date',
             'amount_saldo',
             DB::raw('CASE WHEN code_group < 200000 THEN amount_debet - amount_kredit ELSE
             amount_kredit - amount_debet END as amount_journal'),
             'journal_identifier',
-            DB::raw('coalesce(LAG(amount_saldo) OVER (PARTITION BY code_group ORDER BY index_date), 0) as last_saldo')
-        )->chunkById(2000, function ($journals) use (&$lastSaldo,&$problemsLag,&$problemsManual) {
+            DB::raw('LAG(amount_saldo) OVER (PARTITION BY book_journal_id,code_group ORDER BY index_date) as last_saldo')
+        );
+        
+        
+        $fixJournals= Journal::fromSub($fixJournals, 'journals')->orderBy('journal_identifier')->chunkById(100000, function ($journals) use (&$lastSaldo, &$problemsLag, &$problemsManual) {
             foreach ($journals as $journal) {
                 if (!isset($lastSaldo[$journal->code_group])) {
                     $lastSaldo[$journal->code_group] = $journal->amount_saldo;
@@ -51,39 +55,62 @@ class SearchManualJournalProblem extends Command
 
                     $amanLag = true;
                     $amanManual = true;
-                    if ($journal->amount_journal + $journal->last_saldo != $journal->amount_saldo) {
+                    if (abs(($journal->amount_journal + $journal->last_saldo) - $journal->amount_saldo) > 1) {
                         $amanLag = false;
                     }
-                    if ($journal->amount_journal + $lastSaldo[$journal->code_group] != $journal->amount_saldo) {
+                    if (abs(($journal->amount_journal + $lastSaldo[$journal->code_group]) - $journal->amount_saldo) > 1) {
                         $amanManual = false;
                     }
                     $lastSaldo[$journal->code_group] += $journal->amount_journal;
                     if ($problemsManual[$journal->code_group] === null && $amanManual === false) {
+                        $journal->manual_seharusnya = $journal->amount_journal + $lastSaldo[$journal->code_group];
                         $problemsManual[$journal->code_group] = $journal;
                     }
                     if ($problemsLag[$journal->code_group] === null && $amanLag === false) {
+                        $journal->lag_seharusnya = $journal->amount_journal + $journal->last_saldo;
                         $problemsLag[$journal->code_group] = $journal;
                     }
-                    $strProblemLag = $amanLag ? '✅' : '❌';
-                    $strProblemManual = $amanManual ? '✅' : '❌';
-                    $this->info("Journal: {$journal->journal_identifier} | Problem Lag: {$strProblemLag} | Problem Manual: {$strProblemManual}");
+                    // $strProblemLag = $amanLag ? '✅' : '❌';
+                    // $strProblemManual = $amanManual ? '✅' : '❌';
+                    // $this->info("Journal: {$journal->journal_identifier} | Problem Lag: {$strProblemLag} | Problem Manual: {$strProblemManual}");
                 }
             }
         }, 'journal_identifier');
 
         $this->info('Summary Problem');
         $this->info('Problem Lag:');
-        foreach ($problemsLag as $codeGroup => $problem) {
-            if ($problem !== null) {
-                $this->info("Code Group: {$codeGroup} | Journal: {$problem->journal_identifier} | Index Date: {$problem->index_date} | Amount Journal: {$problem->amount_journal} | Last Saldo: {$problem->last_saldo} | Amount Saldo: {$problem->amount_saldo}");
-            }
-        }
-
-        $this->info('\nProblem Manual:');
-        foreach ($problemsManual as $codeGroup => $problem) {
-            if ($problem !== null) {
-                $this->info("Code Group: {$codeGroup} | Journal: {$problem->journal_identifier} | Index Date: {$problem->index_date} | Amount Journal: {$problem->amount_journal} | Last Saldo: {$lastSaldo[$codeGroup]} | Amount Saldo: {$problem->amount_saldo}");
-            }
-        }
+        $plags = collect($problemsLag)->filter(function ($problem) {
+            return $problem !== null;
+        })->values();
+        tampilkanTableTerminal(
+            $plags,
+            [
+                'code_group' => 'center',
+                'index_date' => 'center',
+                'amount_journal' => 'right',
+                'lag_seharusnya' => 'right',
+                'amount_saldo' => 'right',
+                'last_saldo' => 'right'
+            ],
+            $this
+        );
+       
+        $this->info("");
+        $this->info('Problem Manual:');
+        $pManuals= collect($problemsManual)->filter(function ($problem) {
+            return $problem !== null;
+        })->values();
+        tampilkanTableTerminal(
+            $pManuals,
+            [
+                'code_group' => 'center',
+                'index_date' => 'center',
+                'amount_journal' => 'right',
+                'manual_seharusnya' => 'right',
+                'amount_saldo' => 'right'
+            ],
+            $this
+        );
+       
     }
 }
