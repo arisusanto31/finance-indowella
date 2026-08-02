09c0886188520f441cb8b9dc56e75b73fa2db9ee
iqomchanif <chanif.qomaruddin@gmail.com>
2026-08-03 02:58:27 +0700

    oke

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -74,10 +74,11 @@ class SalesOrderController extends Controller
         $salesOrderNumber = getInput('sales_order_number');
 
         //cek inv pack yang ga valid, ga ada detailnya itu kan sama aja ga valid ya
-        // $trashSO = SalesOrder::whereBetween('created_at', [$startDate, $endDate]);
-        // $trashSO = SalesOrder::fromSub($trashSO, 'sales_orders')->leftJoin('sales_order_details as detail', 'detail.sales_order_id', '=', 'sales_orders.id')
-        //     ->select(DB::raw('count(detail.id) as total_detail'), 'sales_orders.id')->groupBy('sales_orders.id')->havingRaw('total_detail = 0')->pluck('id');
+        $trashSO = SalesOrder::whereBetween('created_at', [$startDate, $endDate]);
+        $trashSO = SalesOrder::fromSub($trashSO, 'sales_orders')->leftJoin('sales_order_details as detail', 'detail.sales_order_id', '=', 'sales_orders.id')
+            ->select(DB::raw('count(detail.id) as total_detail'), 'sales_orders.id')->groupBy('sales_orders.id')->havingRaw('total_detail = 0')->pluck('id');
         // SalesOrder::whereIn('id', $trashSO)->delete();
+    
 
         $invPackFilter = SalesOrder::whereBetween('created_at', [$startDate, $endDate]);
         if ($salesOrderNumber) {
