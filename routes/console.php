1b1c893d3d4348ddb255a58ca2a4528be5eefe04
iqomchanif <chanif.qomaruddin@gmail.com>
2026-09-03 13:59:15 +0700

    ok

--- routes/console.php
+++ routes/console.php
@@ -10,9 +10,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:fix-problem-saldo 1')->cron('*/11 * * * *');
-Schedule::command('app:fix-problem-saldo 2')->cron('*/9 * * * *');
-Schedule::command('app:recalculate-kartu-stock-request')->cron('* * * * *');
+Schedule::command('app:fix-problem-saldo 1')->cron('*/21 * * * *');
+Schedule::command('app:fix-problem-saldo 2')->cron('*/19 * * * *');
+Schedule::command('app:recalculate-kartu-stock-request')->cron('*/17 * * * *');
 Schedule::command('fill:index-date-invoice-sale')->dailyAt('21:00');
 Schedule::command('app:fill-link-kartu-stock 2')->cron('*/5 * * * *');
 // Schedule::command('app:fill-lawan-journal-id')->hourly();
